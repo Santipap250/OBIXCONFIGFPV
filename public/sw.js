5a8d127269a9514f4cec7dc48e312e4c15cac664
TUIzFPV <santipap350z@gmail.com>
2026-07-15 02:23:46 +0700

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,30 @@
+const CACHE = "obixconfig-fpv-v1";
+const APP_SHELL = ["/", "/manifest.webmanifest", "/icons/icon-192.png"];
 
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.map((key) => key !== CACHE && caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+        return res;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match("/")))
+  );
+});
